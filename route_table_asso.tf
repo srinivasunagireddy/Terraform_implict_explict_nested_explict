72ad27929745eabe3af67137df2faac3868eeb0d
Your Name <you@example.com>
2026-01-15 12:46:24 +0530

    Route table and subnet tf files are modified and changed the code to resolve the errors./

--- route_table_asso.tf
+++ route_table_asso.tf
@@ -1,4 +1,4 @@
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "Public_route_table_asso" {
   subnet_id      = aws_subnet.Public_Subnet_Terra.id
   route_table_id = aws_route_table.RT_Terra.id
 }
--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "Public_Subnet_Terra" {
-  vpc_id                          = aws_vpc.Vpc_Terra.id
-  cidr_block                      = "10.0.1.0/24"
-  map_customer_owned_ip_on_launch = true
-  availability_zone               = "us-east-1a"
+  vpc_id                  = aws_vpc.Vpc_Terra.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name    = "Subnet_Terra"
